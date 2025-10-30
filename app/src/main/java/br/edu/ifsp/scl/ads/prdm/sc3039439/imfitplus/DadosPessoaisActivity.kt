664f618f98a0b13fcc10452fbf387a1168f69c2a
LucasLotumolo <lucasblotumolo@gmail.com>
2025-10-30 20:13:39 -0300

    feat: implementando validação para campos de texto dos dados pessoais

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/DadosPessoaisActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/DadosPessoaisActivity.kt
@@ -1,6 +1,7 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.databinding.ActivityDadosPessoaisBinding
 
@@ -13,5 +14,17 @@ class DadosPessoaisActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(adpb.root)
+
+        adpb.calcularImcBt.setOnClickListener {
+            val nome = adpb.nomeEt.text.toString()
+            val idadeStr = adpb.idadeEt.text.toString()
+            val alturaStr = adpb.alturaEt.text.toString()
+            val pesoStr = adpb.pesoEt.text.toString()
+
+            if (nome.isEmpty() || idadeStr.isEmpty() || alturaStr.isEmpty() || pesoStr.isEmpty()) {
+                Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
 }
