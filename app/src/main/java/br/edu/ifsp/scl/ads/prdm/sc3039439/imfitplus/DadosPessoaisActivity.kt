08bcea722b772f813d6d1870396d7896861ca1ca
LucasLotumolo <lucasblotumolo@gmail.com>
2025-10-30 13:59:29 -0300

    feat: criando activity.xml para dados pessoais

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/DadosPessoaisActivity.kt
@@ -0,0 +1,20 @@
+package br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.databinding.ActivityMainBinding
+
+class DadosPessoaisActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+    }
+}
