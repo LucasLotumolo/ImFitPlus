28d5ea06982c3fad883517320fe49b634b3cdc88
LucasLotumolo <lucasblotumolo@gmail.com>
2025-10-30 20:34:41 -0300

    feat: criando tela para peso ideal

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/PesoIdealActivity.kt
@@ -0,0 +1,19 @@
+package br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.databinding.ActivityPesoIdealBinding
+
+class PesoIdealActivity : AppCompatActivity() {
+    private val apib: ActivityPesoIdealBinding by lazy {
+        ActivityPesoIdealBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apib.root)
+    }
+}
