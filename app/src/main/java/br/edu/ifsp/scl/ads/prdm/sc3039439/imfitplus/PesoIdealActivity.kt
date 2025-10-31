8a8fe549281aeb94282485dedbd43fe45fe59530
LucasLotumolo <lucasblotumolo@gmail.com>
2025-10-31 00:25:05 -0300

    feat: lógica implementa para cálculo do peso ideal e da diferença em relação ao peso atual

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/PesoIdealActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/PesoIdealActivity.kt
@@ -1,10 +1,7 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.databinding.ActivityPesoIdealBinding
 
 class PesoIdealActivity : AppCompatActivity() {
@@ -15,5 +12,24 @@ class PesoIdealActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(apib.root)
+
+        val altura = intent.getDoubleExtra("altura", 0.0)
+        val peso = intent.getDoubleExtra("peso", 0.0)
+
+        val pesoIdeal = 22 * (altura * altura)
+        val diferenca = peso - pesoIdeal
+
+        apib.pesoIdealTv.text = String.format("Peso Ideal: %.2f kg", pesoIdeal)
+        apib.diferencaPesoTv.text = String.format("Diferença: %.2f kg", diferenca)
+
+        val recomendacao = when {
+            diferenca > 3 -> "Tente reduzir seu peso com exercícios e alimentação equilibrada."
+            diferenca < -3 -> "Você pode ganhar um pouco de massa com treinos e boa alimentação."
+            else -> "Parabéns! Seu peso está dentro do ideal."
+        }
+
+        apib.recomendacaoTv.text = recomendacao
+
+        apib.voltarBt.setOnClickListener { finish() }
     }
 }
