9cd17480e37aebcb47185dd30e592658b262913b
LucasLotumolo <lucasblotumolo@gmail.com>
2025-12-09 20:42:04 -0300

    feat: implementado tela de histórico

---
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/ui/HistoricoActivity.kt
@@ -0,0 +1,21 @@
+package br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.R
+
+class HistoricoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_historico)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
