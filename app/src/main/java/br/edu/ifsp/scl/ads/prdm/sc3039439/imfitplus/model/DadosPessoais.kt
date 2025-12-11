c74f783deaa3e5ef03abd3da60b8ece954a2c43f
LucasLotumolo1 <lucasblotumolo@gmail.com>
2025-12-10 23:33:39 -0300

    chore: removendo room do projeto

--- app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/model/DadosPessoais.kt
+++ app/src/main/java/br/edu/ifsp/scl/ads/prdm/sc3039439/imfitplus/model/DadosPessoais.kt
@@ -1,14 +1,11 @@
 package br.edu.ifsp.scl.ads.prdm.sc3039439.imfitplus.model
 
 import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-@Entity("usuarios")
 data class DadosPessoais(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0L,
+    val id: Int? = -1,
     val nome: String,
     val idade: Int,
     val altura: Double,
